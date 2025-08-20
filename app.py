bebfd84e2d418f63d8f7a988def9b65a863c3075
Reshma <37250413+reshmaharidhas@users.noreply.github.com>
2025-08-20 22:41:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=["POST","GET"])
 def home():
-    return "BMI CALCULATOR"
+    if request.method=="POST":
+        user_weight_in_kg = request.form.get("weight_input")
+        user_height_in_m = request.form.get("height_input")
+        if len(user_weight_in_kg)>0 and len(user_height_in_m)>0:
+            bmi = str(float(user_weight_in_kg)/(float(user_height_in_m)*float(user_height_in_m)))
+            bmi_num = float(bmi)
+            bmi_status = ""
+            if bmi_num<18.5:
+                bmi_status = "Underweight"
+            elif bmi_num>=18.5 and bmi_num<25:
+                bmi_status = "Normal"
+            elif bmi_num>=25 and bmi_num<30:
+                bmi_status = "Overweight"
+            elif bmi_num>=30:
+                bmi_status = "Obese"
+            return render_template("index.html",currentBMI=bmi[0:5],statusBMI=bmi_status)
+        else:
+            return render_template("index.html",currentBMI="Invalid data",statusBMI="")
+    else:
+        return render_template("index.html")
 
-@app.route('/<randompage>')
-def open_randompage(randompage):
-    return "Page not available!"
+@app.route('/<dynamicPage>')
+def open_dynamicpage(dynamicPage):
+    return f"<h1>Error 404<br>Page not found</h1>"
 
 if __name__=="__main__":
-
     app.run(debug=True)
